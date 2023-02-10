130e0b74f36337979e8a49e00fad2d6c7eb4c7ea
Evgenii Kolesnikov <jekow@yandex.ru>
2023-02-11 00:33:36 +0300

    fix: fix code

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,23 @@ import logo from './logo.svg';
 import './App.scss';
 import SpeedMatch from './Games/GameSpeedMatch/SpeedMatch';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <div>
+        <Link to="/" style={{ color: 'white' }}>
+          Link - /
+        </Link>
+      </div>
+      <div>
+        <Link to="/speedmatch" style={{ color: 'white' }}>
+          Link - /speedmatch
+        </Link>
+      </div>
       <Routes>
+        <Route path="/" />
         <Route path="/speedmatch" element={<SpeedMatch />} />
       </Routes>
     </BrowserRouter>
